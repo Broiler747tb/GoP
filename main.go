386b6ca10db8aeb294a3112b5dc0d7dbdc7ea164
Daniel Kosovskiy <churkabes420@gmail.com>
2025-07-20 15:55:03 +0300

    Made Bin struct for user info and added createUser function

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import "time"
+
 func main() {
 
+}
+
+type Bin struct {
+	id        string
+	private   bool
+	createdAt time.Time
+	name      string
+}
+
+func createUser(id string, private bool, createdAt time.Time, name string) *Bin {
+	return &Bin{
+		id:        id,
+		private:   private,
+		createdAt: createdAt,
+		name:      name,
+	}
 }
